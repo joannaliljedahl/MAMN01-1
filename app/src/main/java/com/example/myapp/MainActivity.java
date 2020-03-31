7fd921559bf102b77add1c5343145b987d6a325d
joanna <joannalh@live.se>
2020-03-31 09:56:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    //opens new activity - Compass
+    public void compassButton(View view){
+        Intent intent = new Intent(this,Compass.class);
+        startActivity(intent);
+    }
+}
